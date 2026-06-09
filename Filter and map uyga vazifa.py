3f5e029d0b646a9b4b57f3a736bfe42008ee87be
alibekodamboyev98-ui <alibekodamboyev98@gmail.com>
2026-06-09 14:23:48 +0300

    Update Filter and map uyga vazifa.py
    Qod

--- Filter and map uyga vazifa.py
+++ Filter and map uyga vazifa.py
@@ -1,15 +1,19 @@
 # 1-masala:
 sozlar = ["olma", "anor", "gilos", "banan"]
 uzunliklar = list(map(len, sozlar))
+print(uzunliklar)
 
 # 2-masala:
 sonlar = [1, 25, 300, 45]
 satrlar = list(map(str, sonlar))
+print(satrlar)
 
 # 3-masala:
 sozlar_unli = ["olma", "anor", "behi", "uzum", "anor", "nok"]
 unli_boshlanuvchi = list(filter(lambda s: s[0].lower() in 'aeiouo‘u‘', sozlar_unli))
+print(unli_boshlanuvchi)
 
 # 5-masala:
 ismlar = ["Ali", "Vali", "Gani"]
-janob_ismlar = list(map(lambda ism: "Mr. " + ism, ismlar))
+Mr_ismlar = list(map(lambda ism: "Mr. " + ism, ismlar))
+print(Mr_ismlar)
